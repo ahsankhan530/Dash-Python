10cdedb4be97f6e6f8cb82f4e643bc44e60d4244
ahsankhan530 <khanahsan530@gmail.com>
2019-04-05 22:10:55 +0500

    dash update

--- index.py
+++ index.py
@@ -4,10 +4,7 @@ import pandas as pd
 import dash
 import csv
 
-# app =dash.Dash()
 
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
 from flask import Flask,redirect,url_for,request
 import flask
 from werkzeug.serving import run_simple
@@ -24,58 +21,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = b'dE\xad2g\x0c\x8d\xb9\x1cq\x86\x04:\xa8>\xc7\xc5\xc2Dr\xe7f\xf9\xeb'
 
 
-dash_app1 = Dash(__name__, server = app)
 df=pd.read_csv('a.csv')
 df.columns=['a','b']
 x=df['a']
 y=df['b']
+print(x,y)
 dash_app2 = Dash(__name__, server = app, url_base_pathname='/reports/')
-dash_app1.layout = html.Div(
-    children=[
-        html.Div(
-            className="container",
-            children=[
-                dcc.Location(id='url_login', refresh=True),
-                html.Div('''Please log in to continue:''', id='h1'),
-                html.Div(
-                    # method='Post',
-                    children=[
-                        dcc.Input(
-                            placeholder='Enter your username',
-                            type='text',
-                            id='uname-box'
-                        ),
-                        dcc.Input(
-                            placeholder='Enter your password',
-                            type='password',
-                            id='pwd-box'
-                        ),
-                        html.Button(
-                            children='Login',
-                            n_clicks=0,
-                            type='submit',
-                            id='login-button'
-                        ),
-                        html.Div(children='', id='output-state')
-                    ]
-                ),
-            ]
-        )
-    ]
-)
+
 
 dash_app2.layout= html.Div(children=[
     html.H1(children='Data visualization'),
     dcc.Graph(
-    id='example',
+    id='example',animate=True,
     figure={
         
             'data': [
                 
-                {'x':x,'y':y,'type':'bar'}
+                {'x':y,'y':x,'type':'bar'}
             ],
             'layout': {
-                'title': 'Dreamer'
+                'title': 'Month and Amount'
             }
         }
     )
@@ -100,48 +65,29 @@ y=0
 @app.route('/',methods=['POST'])
 def index():
     data=request.get_json()
-#     scope=['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-#     credationals=ServiceAccountCredentials.from_json_keyfile_name('placeapi-a3c5e000d8e2.json',scope)
-#     gc=gspread.authorize(credationals)
-#     wks=gc.open('ahsan').sheet1
-#     g=wks.get_all_values()
-    
     a=dreamer(Ref=data["Ref"],Month=data['Month'],Date=data["Date"],Amount=data["Amount"],Detailed_Category=data['Detailed_Category'],
     subcategory=data['subcategory'], Ad=data['Ad'],Paid=data['Paid'],Settled=data['Settled'],FIELD2=data['FIELD2'])
     db.session.add(a)
     db.session.commit()
-    
+   
     return 'ok'
-@app.route('/aa',methods=['POST','GET'])
+@app.route('/reports',methods=['POST','GET'])
 def add():
     f = open("a.csv", "w")
     f.truncate()
     f.close()
     a= dreamer.query.filter_by().all()
-    
     db.session.commit() 
     for i in a:
-        df=pd.Series([i.Date,i.Month])
+        df=pd.Series([i.Amount,i.Month])
         with open('a.csv','a')as f:
             writer = csv.writer(f)
             writer.writerow(df)
-#     f.close()
-        print(df)
-        
-        # df=pd.Series([i.Ref,i.Month,i.Date,i.Amount,i.category,i.subcategory,i.Detailed_Category,i.Ad,i.Paid,i.Settled,i.FIELD2])
-    # x=df[0]
-    # y=df[1]
+            f.close()
     return 'ok'
-    # return flask.redirect('/reports')
-    # df=pd.Series([i.Ref,i.Month,i.Date,i.Amount,i.category,i.subcategory,i.Detailed_Category,i.Ad,i.Paid,i.Settled,i.FIELD2])
    
-     
-# @app.route('/reports')
-# def render_reports():
-#     return flask.redirect('/dash2')
 
 app = DispatcherMiddleware(app, {
-    '/dash1': dash_app1.server,
     '/dash2': dash_app2.server
 })
 
