a83be05013e6dfeec7e95adfa0d6c1723f474ca3
ahsankhan530 <khanahsan530@gmail.com>
2019-04-09 18:54:04 +0500

    ok

--- index.py
+++ index.py
@@ -21,30 +21,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = b'dE\xad2g\x0c\x8d\xb9\x1cq\x86\x04:\xa8>\xc7\xc5\xc2Dr\xe7f\xf9\xeb'
 
 
-df=pd.read_csv('a.csv')
-df.columns=['a','b']
-x=df['a']
-y=df['b']
-print(x,y)
-dash_app2 = Dash(__name__, server = app, url_base_pathname='/reports/')
 
-
-dash_app2.layout= html.Div(children=[
-    html.H1(children='Data visualization'),
-    dcc.Graph(
-    id='example',animate=True,
-    figure={
-        
-            'data': [
-                
-                {'x':y,'y':x,'type':'bar'}
-            ],
-            'layout': {
-                'title': 'Month and Amount'
-            }
-        }
-    )
-])
 db = SQLAlchemy(app)
 
 class dreamer(db.Model):
@@ -60,9 +37,8 @@ class dreamer(db.Model):
      Paid= db.Column(db.String)
      Settled = db.Column(db.String)
      FIELD2 = db.Column(db.String)
-x=0
-y=0
-@app.route('/',methods=['POST'])
+
+@app.route('/e',methods=['POST'])
 def index():
     data=request.get_json()
     a=dreamer(Ref=data["Ref"],Month=data['Month'],Date=data["Date"],Amount=data["Amount"],Detailed_Category=data['Detailed_Category'],
@@ -73,23 +49,54 @@ def index():
     return 'ok'
 @app.route('/reports',methods=['POST','GET'])
 def add():
-    f = open("a.csv", "w")
-    f.truncate()
-    f.close()
-    a= dreamer.query.filter_by().all()
-    db.session.commit() 
-    for i in a:
-        df=pd.Series([i.Amount,i.Month])
-        with open('a.csv','a')as f:
-            writer = csv.writer(f)
-            writer.writerow(df)
-            f.close()
-    return 'ok'
    
+    global a
+    add.a= dreamer.query.filter_by().all()
+    db.session.commit() 
+    # for i in a:
+    #     global df
+    #     add.df=pd.Series([i.Amount,i.Month])
+       
+    return 'df'
+add()
+# print(add.df)
+# df=add.df
+dash_app2 = Dash(__name__, server = app)
+
+# print(add.a)
+li=[]
+ai=[]
+for i in add.a:
+    # df=pd.Series([i.Amount,i.Month])
+    # a=df
+    li.append(i.Month)
+    ai.append(i.Amount)
+ap={'a':li,'pi':ai}
+df=pd.DataFrame(ap)
+y=df['a']
+x=df['pi']
+
+
+
+dash_app2.layout= html.Div(children=[
+    html.H1(children='Data '),
+    dcc.Graph(
+    id='example',animate=True,
+    figure={
+        
+            'data': [
+                
+                {'x':y,'y':x,'type':'bar'}
+            ],
+            'layout': {
+                'title': 'Month and Amount'
+            }
+        }
+    )
+])   
 
 app = DispatcherMiddleware(app, {
     '/dash2': dash_app2.server
 })
 
-db.create_all()
 run_simple('0.0.0.0', 8080, app, use_reloader=True, use_debugger=True)
